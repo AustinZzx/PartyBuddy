ad1390c5d4892c3cc68c299e66cd67e10c95c968
Jingxing Yang <jingxiny@usc.edu>
2017-04-22 12:38:57 -0600

    updated party

--- src/data/Party.java
+++ src/data/Party.java
@@ -1,5 +1,51 @@
 package data;
 
-public class Party {
+import java.util.Vector;
 
+public class Party {
+	private String name;
+	private String location;
+	private int longitude;
+	private int latitude;
+	private User host;
+	Vector<User> attenders;
+	
+	public Party(String name, String location,int longitude,int latitude, String host)
+	{
+		this.name = name;
+		this.location = location;
+		this.longitude = longitude;
+		this.latitude = latitude;
+		this.host = host;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public String location()
+	{
+		return location;
+	}
+	
+	public int getLongitude()
+	{
+		return longitude;
+	}
+	
+	public int getLatitude()
+	{
+		return latitude;
+	}
+	
+	public User getHost()
+	{
+		return host;
+	}
+	
+	public Vector<User> getAttenders()
+	{
+		return attenders;
+	}
 }
