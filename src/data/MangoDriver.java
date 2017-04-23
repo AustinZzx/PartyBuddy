f60659228517a5f25d92048ca533bdf6e59917a6
AustinZzx <zexiazha@usc.edu>
2017-04-23 00:36:13 -0600

    update

--- src/data/MangoDriver.java
+++ src/data/MangoDriver.java
@@ -1,5 +1,19 @@
 package data;
 
-public class MangoDriver {
+import com.mongodb.*;
 
+public class MangoDriver {
+	private Mongo mg;
+	private DB db;
+	private DBCollection userCollection, partyCollection;
+	
+	public void addUser(User u)
+	{
+		userCollection.insert( { name: u.getUsername(), age: u.getUsername() } );
+	}
+	public User retrieveUser(String username)
+	{
+		var cursor = db.users.find( { name: 2 } ); 
+		
+	}
 }
